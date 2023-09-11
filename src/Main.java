58fbd0e82d9fd0f964379d636007cf30c2f4db1d
cmmarques22 <carlos.marques22@live.com>
2023-09-11 10:56:11 +0100

    checkup4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Sudoku!");
-        System.out.println("Hello git3");
+        System.out.println("Hello git4");
     }
 }
