3e22e7b746d425b2eb119e8455542f71f4668e25
cmmarques22 <carlos.marques22@live.com>
2023-09-12 16:10:17 +0100

    SudokuApp

---
+++ src/sudoku/Main.java
@@ -0,0 +1,9 @@
+package sudoku;
+
+import sudoku.problemdomain.SudokuGame;
+
+public class Main {
+    public static void main(String[] args) {
+        SudokuApp.main(new String[]{});
+    }
+}
--- src/sudoku/SudokuApp.java
+++ src/sudoku/SudokuApp.java
@@ -1,4 +1,8 @@
-public class Main {
+package sudoku;
+
+public class SudokuApp  extends Application{
+    private IuserinterfaceContract.View uiImpl;
+
     public static void main(String[] args) {
         System.out.println("Hello Sudoku!");
         System.out.println("Hello git4");
