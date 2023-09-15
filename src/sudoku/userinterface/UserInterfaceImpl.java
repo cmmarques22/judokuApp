0d1c226402cff359eae8222c168952c03bc39308
cmmarques22 <carlos.marques22@live.com>
2023-09-15 11:54:01 +0100

    initializeUserInterface

--- src/sudoku/userinterface/UserInterfaceImpl.java
+++ src/sudoku/userinterface/UserInterfaceImpl.java
@@ -3,30 +3,63 @@ package sudoku.userinterface;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import sudoku.problemdomain.Coordinates;
 import sudoku.problemdomain.SudokuGame;
 
 import java.util.HashMap;
 
-public class UserInterfaceImpl implements IUserInterfaceContract.View {
-    EventHandler<KeyEvent> {
+public class UserInterfaceImpl implements IUserInterfaceContract.View, EventHandler<KeyEvent> {
 
-        private final Stage stage;
-        private final Group root;
+    private final Stage stage;
+    private final Group root;
 
-        //how to track all text fields
-        private HashMap<Coordinates, SudokuTextField> textFieldCoordinates;
-        private IUserInterfaceContract.EventListener listener;
+    //how to track all text fields
+    private HashMap<Coordinates, SudokuTextField> textFieldCoordinates;
+    private IUserInterfaceContract.EventListener listener;
 
-        private static final double WINDOW_Y = 732;
-        private static final double WINDOW_x = 668;
-        private static final double BOARD_PADDING = 50;
-        private static final double BOARD_X_AND_Y = 576;
+    private static final double WINDOW_Y = 732;
+    private static final double WINDOW_x = 668;
+    private static final double BOARD_PADDING = 50;
+    private static final double BOARD_X_AND_Y = 576;
 
+    private static final Color WINDOW_BACKGROUND_COLOR = Color.rgb(0, 10, 136);
+    private static final Color BOARD_BACKGROUND_COLOR = Color.rgb(224, 242, 241);
 
+    private static final String SUDOKU = "Sudoku";
 
+    public UserInterfaceImpl(Stage stage) {
+        this.stage = stage;
+        this.root = new Group();
+        this.textFieldCoordinates = new HashMap<>();
+        initializeUserInterface();
+    }
 
+    private initializeUserInterface() {
+        drawBackgroudnd(root);
+        drawTitle(root);
+        drawSudokuBoard(root);
+        drawTextFields(root);
+        drawGridLines(root);
+        stage.show();
+
+}
+
+    private void drawGridLines(Group root) {
+
+    }
+
+    private void drawTextFields(Group root) {
+    }
+
+    private void drawSudokuBoard(Group root) {
+    }
+
+    private void drawTitle(Group root) {
+    }
+
+    private void drawBackgroudnd(Group root) {
 
     }
 
@@ -55,4 +88,8 @@ public class UserInterfaceImpl implements IUserInterfaceContract.View {
 
     }
 
+    @Override
+    public void handle(KeyEvent keyEvent) {
+
     }
+}
