b1af2148f0b0627fc91100468ebb303b95916441
cmmarques22 <carlos.marques22@live.com>
2023-10-17 15:48:08 +0100

    added build logic comments

--- src/sudoku/buildlogic/SudokuBuildLogic.java
+++ src/sudoku/buildlogic/SudokuBuildLogic.java
@@ -11,6 +11,9 @@ import java.io.IOException;
 
 public class SudokuBuildLogic {
 
+    //class takes in the uiImpl object which is tightly-coupled to the JavaFX framework,
+    // binds that object to the various other objects necessary for the application to function
+
 
     public static void build(IUserInterfaceContract.View userInterface) throws IOException {
         SudokuGame initialState;
@@ -18,10 +21,13 @@ public class SudokuBuildLogic {
 
 
         try {
+            // throw if no game data is found in local storage
             initialState = storage.getGameData();
         } catch (IOException e) {
+            //throw an IOExceptionif we cannot update the game data.
             initialState = GameLogic.getNewGame();
             storage.updateGameData(initialState);
+
         }
 
         IUserInterfaceContract.EventListener uiLogic = new ControlLogic(storage, userInterface);
